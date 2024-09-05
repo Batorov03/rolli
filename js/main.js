6f29debd4618814514709ddb310e16aa46ec2bf6
Chimit <itishn1k.19@gmail.com>
2024-09-05 13:46:44 +0800

    new commit

--- js/main.js
+++ js/main.js
@@ -43,9 +43,25 @@ window.addEventListener('click', function (event) {
 		if (parseInt(counter.innerText) > 1) {
 			// Изменяем текст в счетчике уменьшая его на 1
 			counter.innerText = --counter.innerText;
+		} else if (event.target.closest('.cart-wrapper') && parseInt(counter.innerText) === 1) {
+			// Проверка на товар который находится в корзине
+			console.log('IN CART!!!!');
+			// Удаляем товар из корзины
+			event.target.closest('.cart-item').remove();
+
+			// Отображение статуса корзины Пустая / Полная
+			toggleCartStatus();
+
+			// Пересчет общей стоимости товаров в корзине
+			calcCartPriceAndDelivery();
 		}
 	};
-})
+	// Проверяем клик на + или - внутри коризины
+	if (event.target.hasAttribute('data-action') && event.target.closest('.cart-wrapper')) {
+		// Пересчет общей стоимости товаров в корзине
+		calcCartPriceAndDelivery();
+	}
+});
 
 
 // Div внутри корзины, в который мы добавляем товары
